fe753c97c3c0f5da79057defc6527ba35daf48d3
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-23 15:11:59 +0200

    Project setup

---
+++ FunctionPlan/Domain/Common/DomainException.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Common
+{
+    internal class DomainException
+    {
+    }
+}
---
+++ FunctionPlan/Domain/Common/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Common
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+
+        //Constructor for ef core
+        protected Entity() 
+        {
+        }
+
+        protected Entity(int id) {
+            Id = id;
+        }
+    }
+}

53e88b6155b22b85c611ebd01c0d1bc2afe2e6af
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-24 15:28:34 +0200

    (feat): Added validation for meeting

---
+++ FunctionPlan/Application/Meetings/Commands/CreateMeetingCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Application.Meetings.Commands
+{
+    public sealed class CreateMeetingCommandValidator: AbstractValidator<CreateMeetingCommand>
+    {
+        public CreateMeetingCommandValidator() 
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Meeting title cannot be empty")
+                .MaximumLength(100).WithMessage("Incorrect title content");
+
+            RuleFor(x => x.ScheduledFor)
+                .GreaterThan(DateTime.UtcNow).WithMessage("Incorrect meeting date");
+
+            RuleFor(x => x.Latitude)
+                .InclusiveBetween(-90, 90).WithMessage("Incorrect Latitude");
+
+            RuleFor(x => x.Longitude)
+                .InclusiveBetween(-180, 180).WithMessage("Incorrect Longitude");
+        }
+    }
+}

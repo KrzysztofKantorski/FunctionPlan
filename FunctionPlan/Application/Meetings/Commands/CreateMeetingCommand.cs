9927614e0f420eeb35dfd84da63ac7cf7dec2e7f
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-24 14:55:16 +0200

    (feat): Implemented AddAsync method

---
+++ FunctionPlan/Application/Meetings/Commands/CreateMeetingCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Application.Meetings.Commands
+{
+    public sealed record CreateMeetingCommand(
+        string Title,
+        DateTime ScheduledFor,
+        int OrganizerId,
+        double Latitude,
+        double Longitude
+    ): IRequest<int>;
+
+}
---
+++ FunctionPlan/Application/Meetings/Commands/CreateMeetingCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Domain.Common;
+using Domain.Meetings;
+namespace Application.Meetings.Commands
+{
+    internal sealed class CreateMeetingCommandHandler: IRequestHandler<CreateMeetingCommand, int>
+    {
+        private readonly IMeetingRepository _meetingRepository;
+
+        public CreateMeetingCommandHandler(IMeetingRepository meetingRepository)
+        {
+           _meetingRepository = meetingRepository;
+        }
+
+        public async Task<int> Handle(CreateMeetingCommand request, CancellationToken cancellationToken)
+        {
+            var location = new Coordinates(request.Latitude, request.Longitude);
+
+            var meeting = new Meeting(
+                request.Title,
+                request.ScheduledFor,
+                request.OrganizerId,
+                location
+            );
+
+            await _meetingRepository.AddAsync( meeting );
+
+            return meeting.Id;
+        }
+    }
+}

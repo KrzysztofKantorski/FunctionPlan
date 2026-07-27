ae5509ffbf7e84a1168fbb3de8049d57eb181808
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-27 15:05:48 +0200

    (feat): Added pipeline validation behavior

--- FunctionPlan/Application/Abstractions/Messaging/Messaging.cs
+++ FunctionPlan/Application/Abstractions/Messaging/Messaging.cs
@@ -6,7 +6,7 @@ namespace Application.Abstractions.Messaging
     {
 
     }
-    public interface ICommand: IRequest
+    public interface ICommand: IRequest, ICommandBase
     {
 
     }
---
+++ FunctionPlan/Application/Exceptions/validationException.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Exceptions
+{
+    public class ValidationException: ApplicationException
+    {
+        public IEnumerable<ValidationError> Errors { get; }
+        public ValidationException(IEnumerable<ValidationError> errors) 
+        {
+            Errors = errors;
+        }
+    }
+}

b23202273e8f471ca1d3ed59e9f1c0204d12b05f
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-27 17:50:53 +0200

    (feat): Add packages

---
+++ FunctionPlan/Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Application.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            //MediatR
+            services.AddMediatR(config => {
+                config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            //FluentValidation
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            return services;
+        }
+    }
+}
---
+++ FunctionPlan/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Common;
+using Domain.Meetings;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("CONN_STRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string not found");
+            }
+
+            //Register DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(connectionString));
+
+
+            services.AddScoped<IMeetingRepository, MeetingRepository>();
+            services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+            return services;
+        }
+    }
+}

7d90da3503031f6fadab287104aae1d845bfac22
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-27 16:20:06 +0200

    (fix): Use Set instead of DbSet

--- FunctionPlan/Infrastructure/Persistence/ApplicationDbContext.cs
+++ FunctionPlan/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -7,8 +7,6 @@ namespace Infrastructure.Persistence
     public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
-
-        public DbSet<Meeting> Meetings { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);

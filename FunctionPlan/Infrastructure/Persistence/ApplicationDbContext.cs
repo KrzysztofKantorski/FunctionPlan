99f461aa107431aee9ac3d0e88c47f684ce1a07b
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-27 16:07:06 +0200

    (feat): Infrastructure DbContext setup

---
+++ FunctionPlan/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using Domain.Meetings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Meeting> Meetings { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}

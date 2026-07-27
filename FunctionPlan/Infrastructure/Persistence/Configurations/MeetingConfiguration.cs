26e5b26b279301881e4103998c0bf8c9f259a0ee
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-27 16:13:59 +0200

    (feat): Meeting configuration

---
+++ FunctionPlan/Infrastructure/Persistence/Configurations/MeetingConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Meetings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal sealed class MeetingConfiguration : IEntityTypeConfiguration<Meeting>
+    {
+        public void Configure(EntityTypeBuilder<Meeting> builder)
+        {
+            builder.ToTable("Meetings");
+            builder.HasKey(p => p.Id);
+            builder.Property(m => m.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            //Map Coordinates object to table columns
+            builder.OwnsOne(m => m.Location, locationBuilder =>
+            {
+                locationBuilder.Property(c => c.Latitude)
+                .HasColumnName("Latitude")
+                .IsRequired();
+
+                locationBuilder.Property(c => c.Longitude)
+                .HasColumnName("Longitude")
+                .IsRequired();
+            }
+            );
+        }
+    }
+}

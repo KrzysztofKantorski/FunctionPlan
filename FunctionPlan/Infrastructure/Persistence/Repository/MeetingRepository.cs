3678df50787c441913fed6e6423852ed423d10a5
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-27 16:37:16 +0200

    (feat): Meeting repository

---
+++ FunctionPlan/Infrastructure/Persistence/Repository/MeetingRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Meetings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repository
+{
+    public sealed class MeetingRepository: IMeetingRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MeetingRepository(ApplicationDbContext dbCcontext) 
+        {
+            _dbContext = dbCcontext;
+        }
+
+        public async Task<IEnumerable<Meeting>> GetByTitleAsync(string title, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<Meeting>()
+                .Where(t => t.Title == title)
+                .ToListAsync(cancellationToken);
+        }
+
+
+        public async Task<Meeting> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<Meeting>()
+                 .FirstOrDefaultAsync(m => m.Id == id, cancellationToken);
+        }
+
+
+        public async Task AddAsync(Meeting meeting, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Set<Meeting>()
+                .AddAsync(meeting, cancellationToken);
+        }
+
+        public void Update(Meeting meeting)
+        {
+            _dbContext.Set<Meeting>().Update(meeting);
+        }
+
+
+        public void Remove(Meeting meeting)
+        {
+            _dbContext.Set<Meeting>().Remove(meeting);
+        }
+    }
+}

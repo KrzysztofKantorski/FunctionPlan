03751ec8b41fe5b8f427e61fff22f73b2e71d715
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-30 14:03:39 +0200

    (feat): Add message for 500 error

--- FunctionPlan/API/Middlewares/GlobalExceptionHandler.cs
+++ FunctionPlan/API/Middlewares/GlobalExceptionHandler.cs
@@ -6,33 +6,58 @@ namespace API.Middlewares
 {
     internal sealed class GlobalExceptionHandler : IExceptionHandler
     {
+        private readonly ILogger _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
         public async ValueTask<bool> TryHandleAsync(
             HttpContext httpContext, 
             Exception exception, 
             CancellationToken cancellationToken)
         {
-            if(exception is ValidationException validationException)
+
+            //Log every unhandled error
+
+            _logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
+
+            //Fluent validation errors
+
+            if (exception is ValidationException validationException)
             {
                 httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
 
-                var problemDetails = new ValidationProblemDetails
+                var validationProblemDetails = new ValidationProblemDetails
                 {
                     Status = StatusCodes.Status400BadRequest,
                     Title = "Validation error"
                 };
+
                 foreach (var error in validationException.Errors)
                 {
-                    problemDetails.Errors.Add(error.PropertyName, new[] { error.ErrorMessage });
+                    validationProblemDetails.Errors.Add(error.PropertyName, new[] { error.ErrorMessage });
                 }
 
-                await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+                await httpContext.Response.WriteAsJsonAsync(validationProblemDetails, cancellationToken);
                 return true;
             }
 
-            else
+
+            //Server errors
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var problemDetails = new ProblemDetails
             {
-                return false;
-            }
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Server Error",
+                Detail = "An unexpected error occurred while processing your request."
+            };
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+            return true;
         }
     }
 }

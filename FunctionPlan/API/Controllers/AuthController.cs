14c46d886a7c302b8521b647a8e98b968aaa1afb
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-01 00:33:33 +0200

    (feat): Register user

---
+++ FunctionPlan/API/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Auth.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace API.Controllers
+{
+    [Route("api/auth")]
+    [EnableRateLimiting("GlobalLimit")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private ISender _sender;
+        public AuthController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser(
+           [FromBody] RegisterUserCommand command,
+           CancellationToken cancellationToken)
+        {
+            int userId = await _sender.Send(command, cancellationToken);
+            return Created("", new { Id = userId });
+        }
+    }
+}

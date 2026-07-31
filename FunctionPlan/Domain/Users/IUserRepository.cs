1b0d7809fb17a538c7c2ac8862484f545d7433f4
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-31 14:49:00 +0200

    (feat): User domain repository

---
+++ FunctionPlan/Domain/Users/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Users
+{
+    public interface IUserRepository
+    {
+        //Register user
+        Task AddAsync(User user, CancellationToken cancellationToken = default);
+
+        //Get user by email address (email + password auth)
+        Task<User?> GetByEmailAddressAsync(string email, CancellationToken cancellationToken = default);
+
+        //Get user by id
+        Task<User?> GetByGoogleIdAsync(int id, CancellationToken cancellationToken = default);
+    }
+}

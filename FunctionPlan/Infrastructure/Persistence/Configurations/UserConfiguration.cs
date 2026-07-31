979668e2461fbecb6191d3a2b59b781d8788d3be
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-07-31 23:41:36 +0200

    (feat): Implement user repository methods

---
+++ FunctionPlan/Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(m => m.Username)
+               .IsRequired()
+               .HasMaxLength(30);
+
+            builder.Property(m => m.Email)
+               .IsRequired();
+
+            builder.Property(m => m.Role)
+              .IsRequired();
+        }
+    }
+}
---
+++ FunctionPlan/Infrastructure/Persistence/Repository/AuthRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repository
+{
+    internal class AuthRepository: IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AuthRepository(ApplicationDbContext dbCcontext)
+        {
+            _dbContext = dbCcontext;
+        }
+
+
+        public async Task<User?> GetByEmailAddressAsync(string email, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<User>()
+                .FirstOrDefaultAsync(x=> x.Email == email, cancellationToken);
+        }
+
+
+        public async Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<User>()
+                .FirstOrDefaultAsync(x => x.Username == username, cancellationToken);
+        }
+
+
+        public async Task<User?> GetByGoogleSubjectIdAsync(string googleSubjectId, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<User>()
+                .FirstOrDefaultAsync(x=> x.GoogleSubjectId == googleSubjectId, cancellationToken);
+        }
+
+
+        public async Task<User?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<User>()
+                .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+
+        public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Set<User>()
+                .AddAsync(user, cancellationToken);
+        }
+
+
+    }
+}

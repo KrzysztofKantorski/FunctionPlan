fa0694f6691a8eb4a3f3b9595ca84e712532d385
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-04 01:30:18 +0200

    (feat): Refresh tokens configuration

---
+++ FunctionPlan/Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.RefreshTokens;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal sealed class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable("RefreshTokens");
+            builder.HasKey(p => p.Id);
+            builder.Property(m => m.UserId)
+              .IsRequired();
+            builder.Property(m => m.Token)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            //Relation with user
+            builder.HasOne(rt => rt.User)
+                .WithMany() 
+                .HasForeignKey(rt => rt.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}

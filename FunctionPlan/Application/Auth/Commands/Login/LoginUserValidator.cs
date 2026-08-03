609045b93a150fe5f92612c8b5fa8d777387f7af
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-04 01:13:43 +0200

    (feat): Login validator

---
+++ FunctionPlan/Application/Auth/Commands/Login/LoginUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Application.Auth.Commands.Login
+{
+    public sealed class LoginUserValidator: AbstractValidator<LoginUserCommand>
+    {
+        public LoginUserValidator() 
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email address is required")
+                .EmailAddress().WithMessage("Invalid email address");
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .Length(8, 20).WithMessage("Incorrect password");
+        }
+    }
+}

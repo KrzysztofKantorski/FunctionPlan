d61e6e5454f28cdd84704f230a3ec658c7ea94fb
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-05 12:08:51 +0200

    (fix): Too much info from validator

--- FunctionPlan/Application/Auth/Commands/Login/LoginUserValidator.cs
+++ FunctionPlan/Application/Auth/Commands/Login/LoginUserValidator.cs
@@ -7,10 +7,8 @@ namespace Application.Auth.Commands.Login
         public LoginUserValidator() 
         {
             RuleFor(x => x.Email)
-                .NotEmpty().WithMessage("Email address is required")
                 .EmailAddress().WithMessage("Invalid email address");
             RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Password is required")
                 .Length(8, 20).WithMessage("Incorrect password");
         }
     }

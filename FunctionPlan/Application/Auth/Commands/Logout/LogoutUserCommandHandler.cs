359a39ee0f136db8dc68bd3aac663afd91a23d32
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-05 12:44:25 +0200

    (feat): Logout functionality

---
+++ FunctionPlan/Application/Auth/Commands/Logout/LogoutUserCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Common;
+using Domain.RefreshTokens;
+using MediatR;
+
+namespace Application.Auth.Commands.Logout
+{
+    internal class LogoutUserCommandHandler: IRequestHandler<LogoutUserCommand>
+    {
+
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public LogoutUserCommandHandler(IRefreshTokenRepository refreshTokenRepository, IUnitOfWork unitOfWork)
+        {
+            _refreshTokenRepository = refreshTokenRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(LogoutUserCommand request, CancellationToken cancellationToken)
+        {
+            //Check if  refresh token is valid
+            if (request.refreshToken == null)
+            {
+                throw new Exception("Incorrect token");
+            }
+
+            //Check if refresh token exists in db
+            var refreshToken = await _refreshTokenRepository.GetByTokenAsync(request.refreshToken);
+
+            if (refreshToken == null || refreshToken.IsRevoked)
+            {
+                return;
+            }
+
+            refreshToken.Revoke();
+
+            _refreshTokenRepository.Update(refreshToken);
+
+            //Save changes to db
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}

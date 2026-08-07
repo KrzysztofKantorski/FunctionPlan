dfd6bd35743030575547420209e7754062af23e4
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-07 13:40:32 +0200

    (feat): Email sender validator

---
+++ FunctionPlan/Application/Auth/Commands/Email/SendTestEmailValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Application.Auth.Commands.Email
+{
+    public sealed class SendTestEmailValidator : AbstractValidator<SendTestEmailCommand>
+    {
+        public SendTestEmailValidator()
+        {
+            RuleFor(x => x.To)
+                .EmailAddress().WithMessage("Invalid email address");
+            RuleFor(x => x.Subject)
+                .NotEmpty().WithMessage("Subject cannot be empty");
+            RuleFor(x => x.Body)
+                .NotEmpty().WithMessage("Body cannot be empty");
+        }
+    }
+}

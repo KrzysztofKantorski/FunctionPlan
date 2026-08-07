0d462461f2f286a05ca9c75c31164b5403810535
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-07 13:37:15 +0200

    (feat): Email sender handler

---
+++ FunctionPlan/Application/Auth/Commands/Email/SendTestEmailCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Auth.Commands.Email
+{
+    public sealed record SendTestEmailCommand(
+        string To,
+        string Subject,
+        string Body
+    ) : ICommand;
+}
---
+++ FunctionPlan/Application/Auth/Commands/Email/SendTestEmailCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Abstractions.Mail;
+using MediatR;
+
+
+namespace Application.Auth.Commands.Email
+{
+    internal sealed class SendTestEmailCommandHandler : IRequestHandler<SendTestEmailCommand>
+    {
+        private readonly IEmailSender _sender;
+        public SendTestEmailCommandHandler(IEmailSender sender) 
+        { 
+            _sender = sender;
+        }
+        public async Task Handle(SendTestEmailCommand request, CancellationToken cancellationToken)
+        {
+            //Just send email
+            await _sender.SendEmailAsync(request.To, request.Subject, request.Body, cancellationToken);
+        }
+    }
+}

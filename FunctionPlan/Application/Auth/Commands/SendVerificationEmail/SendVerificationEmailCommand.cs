37c2b0786d9a9e4448464fa2e16f11eeb6cf30e1
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-08 15:45:42 +0200

    (feat): Send verification email handler

---
+++ FunctionPlan/Application/Auth/Commands/SendVerificationEmail/SendVerificationEmailCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Auth.Commands.UserVerification
+{
+    public sealed record SendVerificationEmailCommand(
+        string Email):ICommand;
+}
---
+++ FunctionPlan/Application/Auth/Commands/SendVerificationEmail/SendVerificationEmailCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using Application.Abstractions.Cache;
+using Application.Abstractions.Mail;
+using Application.Abstractions.Security;
+using Domain.Users;
+using MediatR;
+namespace Application.Auth.Commands.UserVerification
+{
+    internal sealed class SendVerificationEmailCommandHandler : IRequestHandler<SendVerificationEmailCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IOTPGenerator _otpGenerator;
+        private readonly ICacheService _cacheService;
+        private readonly IEmailSender _emailService;
+
+        public SendVerificationEmailCommandHandler(IUserRepository userRepository, IOTPGenerator otpGenerator,
+            ICacheService cacheService, IEmailSender emailService)
+        {
+            _userRepository = userRepository;
+            _otpGenerator = otpGenerator;
+            _cacheService = cacheService;
+            _emailService = emailService;
+        }
+
+        public async Task Handle(SendVerificationEmailCommand request, CancellationToken cancellationToken)
+        {
+
+            //Check if user is registered
+            var existingUser = await _userRepository.GetByEmailAddressAsync(request.email, cancellationToken);
+
+            if(existingUser == null)
+            {
+                throw new Exception("This user does not exist");
+            }
+
+            //Check if user is verified
+            if(existingUser.IsVerified)
+            {
+                throw new Exception("This user is already verified");
+            }
+
+            //Generate OTP
+            var verificationCode = _otpGenerator.GenerateOTP();
+
+            //Redis key
+            string redisKey = $"otp:verification:user:{existingUser.Id}";
+
+            //Save code and id to redis
+            await _cacheService.SetAsync(redisKey, verificationCode, TimeSpan.FromMinutes(10), cancellationToken);
+
+            //Send email with code to user
+            string subject = "Verification Code";
+            string body = $"Your verification code is: {verificationCode}";
+
+            await _emailService.SendEmailAsync(existingUser.Email, subject, body, cancellationToken);
+        }
+    }
+}

a655526252492126de111d23f03925cc79f53905
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-08 15:49:21 +0200

    (feat): Email sender validator

---
+++ FunctionPlan/Application/Auth/Commands/SendVerificationEmail/SendVerificationEmailCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.Auth.Commands.UserVerification;
+using FluentValidation;
+
+namespace Application.Auth.Commands.SendVerificationEmail
+{
+    public sealed class SendVerificationEmailCommandValidator: AbstractValidator<SendVerificationEmailCommand>
+    {
+        public SendVerificationEmailCommandValidator() {
+            RuleFor(x => x.Email)
+               .EmailAddress().WithMessage("Invalid email address");
+        }
+    }
+}

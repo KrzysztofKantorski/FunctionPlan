276c26acf942a73376e6b757d9e39e5b974c230c
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-08 17:25:33 +0200

    (feat): User registered event

---
+++ FunctionPlan/Application/Auth/Events/UserRegisteredEvent.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Application.Auth.Events
+{
+    public sealed record UserRegisteredEvent
+    (string Email): INotification;
+}
---
+++ FunctionPlan/Application/Auth/Events/UserRegisteredEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using Application.Auth.Commands.UserVerification;
+using MediatR;
+
+namespace Application.Auth.Events
+{
+    internal class UserRegisteredEventHandler : INotificationHandler<UserRegisteredEvent>
+    {
+        private readonly ISender _sender;
+        public UserRegisteredEventHandler(ISender sender)
+        {
+            _sender = sender;
+        }
+        public async Task Handle(UserRegisteredEvent notification, CancellationToken cancellationToken)
+        {
+            var command = new SendVerificationEmailCommand(notification.Email);
+            await _sender.Send(command, cancellationToken);
+        }
+    }
+}

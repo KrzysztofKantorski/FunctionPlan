b308469ff1f4de5c78bd1f9c0370e41daa8dca93
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-09 22:44:52 +0200

    (feat): User email verification validator

---
+++ FunctionPlan/Application/Auth/Commands/VerifyUserEmail/VerifyUserEmailCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Application.Auth.Commands.VerifyUserEmail
+{
+    public sealed class VerifyUserEmailCommandValidator: AbstractValidator<VerifyUserEmailCommand>
+    {
+        public VerifyUserEmailCommandValidator() 
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email address is required")
+                .EmailAddress().WithMessage("Incorrect email address");
+
+            RuleFor(x => x.OTP)
+                .NotEmpty().WithMessage("Verification code is required")
+                .Length(6).WithMessage("Verification code must be exactly 6 characters")
+                .Matches("^[0-9]+$").WithMessage("Verification code must contain only digits");
+        }
+    }
+}

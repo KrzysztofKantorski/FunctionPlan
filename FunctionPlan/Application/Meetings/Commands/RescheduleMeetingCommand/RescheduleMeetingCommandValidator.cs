ceba57c1911effb9b1e2ddae335d3b573e78cebb
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-11 18:26:21 +0200

    (feat): Reschedule meeting validator

---
+++ FunctionPlan/Application/Meetings/Commands/RescheduleMeetingCommand/RescheduleMeetingCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Application.Meetings.Commands.RescheduleMeetingCommand
+{
+    public sealed class RescheduleMeetingCommandValidator: AbstractValidator<RescheduleMeetingCommand>
+    {
+        public RescheduleMeetingCommandValidator()
+        {
+            RuleFor(x => x.MeetingId)
+                .NotEmpty().WithMessage("Meeting id must be provided")
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect meeting id");
+
+            RuleFor(x => x.OrganizerId)
+                .NotEmpty().WithMessage("User id must be provided")
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect user id");
+
+            RuleFor(x => x.ScheduledFor)
+               .GreaterThan(DateTime.UtcNow).WithMessage("Incorrect meeting date");
+        }
+    }
+}

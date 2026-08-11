daf1229e9c42af15a7adfc08dc6979a0f56d9a5d
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-11 23:26:17 +0200

    (feat): Change coordinates endpoint

--- FunctionPlan/API/Controllers/MeetingsController.cs
+++ FunctionPlan/API/Controllers/MeetingsController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Meetings.Commands.CancellMeetingCommand;
+using Application.Meetings.Commands.ChangeCoordinates;
 using Application.Meetings.Commands.CreateMeetingCommand;
 using Application.Meetings.Commands.RescheduleMeetingCommand;
 using Application.Meetings.Queries.GetMeetingById;
@@ -119,5 +120,32 @@ namespace API.Controllers
             return NoContent();
         }
 
+
+        //Change coordinates
+        [HttpPatch("{MeetingId}/coordinates")]
+        public async Task<IActionResult> ChangeMeetingCoordinates(
+            [FromBody] ChangeCoordinatesRequestDto request,
+            [FromRoute] int MeetingId,
+            CancellationToken cancellationToken
+            )
+        {
+            var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            if (!int.TryParse(userId, out int organizerId))
+            {
+                return Unauthorized("Incorrect token");
+            }
+
+            var command = new ChangeCoordinatesCommand(
+                MeetingId,
+                organizerId,
+                request.Longitude,
+                request.Latitude
+            );
+
+            await _sender.Send(command, cancellationToken);
+
+            return NoContent();
+        }
     }
 }

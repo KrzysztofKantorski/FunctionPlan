c7e493fc0197be5f6e49c563fec76f7c2f2f9473
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-11 23:14:12 +0200

    (feat): Change coordinates validator

---
+++ FunctionPlan/Application/Meetings/Commands/ChangeCoordinates/ChangeCoordinatesCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Application.Meetings.Commands.ChangeCoordinates
+{
+    public sealed class ChangeCoordinatesCommandValidator: AbstractValidator<ChangeCoordinatesCommand>
+    {
+        public ChangeCoordinatesCommandValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+               .NotEmpty().WithMessage("Meeting id must be provided")
+               .GreaterThanOrEqualTo(0).WithMessage("Incorrect meeting id");
+
+            RuleFor(x => x.OrganizerId)
+                .NotEmpty().WithMessage("User id must be provided")
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect user id");
+
+            RuleFor(x => x.Latitude)
+               .InclusiveBetween(-90, 90).WithMessage("Incorrect Latitude");
+
+            RuleFor(x => x.Longitude)
+                .InclusiveBetween(-180, 180).WithMessage("Incorrect Longitude");
+        }
+    }
+}

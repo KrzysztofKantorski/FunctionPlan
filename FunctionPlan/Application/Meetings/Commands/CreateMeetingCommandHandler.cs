ccd425c047e3e2088a6d988488546c6165a0cf40
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-11 11:54:01 +0200

    (feat): Check organizer before adding meeting

--- FunctionPlan/Application/Meetings/Commands/CreateMeetingCommandHandler.cs
+++ FunctionPlan/Application/Meetings/Commands/CreateMeetingCommandHandler.cs
@@ -1,21 +1,34 @@
 ﻿using MediatR;
 using Domain.Common;
 using Domain.Meetings;
+using Domain.Users;
+using Application.Exceptions;
 namespace Application.Meetings.Commands
 {
     internal sealed class CreateMeetingCommandHandler: IRequestHandler<CreateMeetingCommand, int>
     {
         private readonly IMeetingRepository _meetingRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IUnitOfWork _unitOfWork;
 
-        public CreateMeetingCommandHandler(IMeetingRepository meetingRepository, IUnitOfWork unitOfWork)
+        public CreateMeetingCommandHandler(IMeetingRepository meetingRepository, IUnitOfWork unitOfWork, IUserRepository userRepository)
         {
             _meetingRepository = meetingRepository;
             _unitOfWork = unitOfWork;
+            _userRepository = userRepository;
         }
 
         public async Task<int> Handle(CreateMeetingCommand request, CancellationToken cancellationToken)
         {
+           
+            //Check if organizer exists
+            var Organizer = await _userRepository.GetByIdAsync( request.OrganizerId, cancellationToken);
+
+            if(Organizer is null)
+            {
+                throw new UserNotFoundException($"Organizer with ID {request.OrganizerId} was not found.");
+            }
+
             var location = new Coordinates(request.Latitude, request.Longitude);
 
             var meeting = new Meeting(

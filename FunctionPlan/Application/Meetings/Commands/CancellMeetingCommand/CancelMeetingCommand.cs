5af8225892856ccd81fc04ee5aae8866fef95f6a
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-11 20:02:20 +0200

    (feat): Cancell meeting

---
+++ FunctionPlan/Application/Meetings/Commands/CancellMeetingCommand/CancelMeetingCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Meetings.Commands.CancellMeetingCommand
+{
+    public sealed record CancelMeetingCommand
+    (
+        int MeetingId,
+        int OrganizerId
+    ): ICommand;
+}
---
+++ FunctionPlan/Application/Meetings/Commands/CancellMeetingCommand/CancelMeetingCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Exceptions;
+using Domain.Common;
+using Domain.Meetings;
+using MediatR;
+
+namespace Application.Meetings.Commands.CancellMeetingCommand
+{
+    internal sealed class CancelMeetingCommandHandler : IRequestHandler<CancelMeetingCommand>
+    {
+        private readonly IMeetingRepository _meetingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public CancelMeetingCommandHandler(IMeetingRepository meetingRepository, IUnitOfWork unitOfWork) 
+        {
+            _meetingRepository = meetingRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(CancelMeetingCommand request, CancellationToken cancellationToken)
+        {
+            var meetingToCancel = await _meetingRepository.GetByIdAsync(request.MeetingId);
+
+            //Check if meeting exists
+            if (meetingToCancel is null)
+            {
+                throw new MeetingNotFoundException("Meeting not found");
+            }
+
+            //Check if user is organizer
+            if (meetingToCancel.OrganizerId != request.OrganizerId) 
+            {
+                throw new UserWithoutPermisionException("You dont have permision to reschedule meeting");
+            }
+
+            meetingToCancel.Cancel();
+
+            _meetingRepository.Update(meetingToCancel);
+
+            await _unitOfWork.SaveChangesAsync();
+
+        }
+    }
+}

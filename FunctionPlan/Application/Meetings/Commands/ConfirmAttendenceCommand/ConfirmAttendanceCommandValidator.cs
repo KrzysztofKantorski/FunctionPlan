119c3a6fd661a92aa2fb097c5b49abd0b51b57ef
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-12 14:45:25 +0200

    (feat): Confirm meeting attemdance validator

---
+++ FunctionPlan/Application/Meetings/Commands/ConfirmAttendenceCommand/ConfirmAttendanceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Meetings.Commands.ConfirmAttendenceCommand
+{
+    public sealed class ConfirmAttendanceCommandValidator: AbstractValidator<ConfirmAttendenceCommand>
+    {
+        public ConfirmAttendanceCommandValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+                   .NotEmpty().WithMessage("Meeting id must be provided")
+                   .GreaterThanOrEqualTo(0).WithMessage("Incorrect meeting id");
+
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("User id must be provided")
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect user id");
+        }
+    }
+}

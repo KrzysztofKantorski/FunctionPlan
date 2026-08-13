ca4f30a6ad262b7fb5e55e5a5743c38159e82099
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-13 10:36:29 +0200

    (feat): Get user id from token

---
+++ FunctionPlan/API/Extensions/UserClaims.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class UserClaims
+    {
+        public static int GetUserId(this ClaimsPrincipal principal)
+        {
+            var userIdStr = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!int.TryParse(userIdStr, out int UserId))
+            {
+                throw new UnauthorizedAccessException("Incorrect or missing token");
+            }
+
+            return UserId;
+        }
+    }
+}

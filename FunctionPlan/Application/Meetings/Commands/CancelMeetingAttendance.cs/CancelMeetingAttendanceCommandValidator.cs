4162a354370893bd58d08046507f4e7da0dd14eb
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-13 10:12:48 +0200

    (feat): Cancel attendance validator

---
+++ FunctionPlan/Application/Meetings/Commands/CancelMeetingAttendance.cs/CancelMeetingAttendanceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Meetings.Commands.CancelMeetingAttendance.cs
+{
+    public sealed class CancelMeetingAttendanceCommandValidator: AbstractValidator<CancelMeetingAttendanceCommand>
+    {
+        public CancelMeetingAttendanceCommandValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+                  .NotEmpty().WithMessage("Meeting id must be provided")
+                  .GreaterThanOrEqualTo(0).WithMessage("Incorrect meeting id");
+
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("User id must be provided")
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect user id");
+        }
+    }
+}

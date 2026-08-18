00f39e0977103f6eac3ed5bdb5402d471f41cd09
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-18 15:30:54 +0200

    (feat): Get meetings dto's

---
+++ FunctionPlan/Application/Meetings/Queries/GetMeetings/GetMeetingsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Meetings.Queries.GetMeetings
+{
+    public sealed record GetMeetingsQuery(
+        string? SearchTerm,
+        string? SortOrder
+    ): ICommand<MeetingListDto>;
+}
---
+++ FunctionPlan/Application/Meetings/Queries/GetMeetings/MeetingListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Meetings.Queries.GetMeetings
+{
+    public sealed record MeetingListDto(
+        int Id,
+        string Title,
+        DateTime ScheduledFor,
+        int OrganizerId
+    );
+}

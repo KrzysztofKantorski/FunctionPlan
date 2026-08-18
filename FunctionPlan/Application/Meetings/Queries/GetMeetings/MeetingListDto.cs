ad04e275b23b4160e35c5635e70e22239d32c2f3
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-18 16:18:58 +0200

    (fix): Change record to class

--- FunctionPlan/Application/Meetings/Queries/GetMeetings/MeetingListDto.cs
+++ FunctionPlan/Application/Meetings/Queries/GetMeetings/MeetingListDto.cs
@@ -1,10 +1,11 @@
 ﻿namespace Application.Meetings.Queries.GetMeetings
 {
-    public sealed record MeetingListDto(
-        int Id,
-        string Title,
-        DateTime ScheduledFor,
-        int OrganizerId,
-        string OrganizerName
-    );
+    public sealed class MeetingListDto
+    {
+        public int Id { get; init; }
+        public string Title { get; init; }
+        public DateTime ScheduledFor { get; init; }
+        public int OrganizerId { get; init; }
+        public string OrganizerName { get; init; }
+    };
 }

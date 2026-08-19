c8fc2648ffee33809b7ce3c99cdf97ded81366af
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-19 14:48:11 +0200

    (feat): Complete meetings handler

---
+++ FunctionPlan/Application/Meetings/Commands/CompletePastMeetingsJob/CompletePastMeetingJob.cs
@@ -0,0 +1,7 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Meetings.Commands.CompletePastMeetingsJob
+{
+    internal sealed record CompletePastMeetingJob
+    ():ICommand;
+}
---
+++ FunctionPlan/Application/Meetings/Commands/CompletePastMeetingsJob/CompletePastMeetingJobHandler.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Common;
+using Domain.Meetings;
+using MediatR;
+
+namespace Application.Meetings.Commands.CompletePastMeetingsJob
+{
+    internal sealed class CompletePastMeetingJobHandler : IRequestHandler<CompletePastMeetingJob>
+    {
+        private readonly IMeetingRepository _meetingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompletePastMeetingJobHandler(IMeetingRepository meetingRepository, IUnitOfWork unitOfWork)
+        {
+            _meetingRepository = meetingRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(CompletePastMeetingJob request, CancellationToken cancellationToken)
+        {
+
+            var cutoffTime = DateTime.UtcNow.AddHours(-2);
+
+            //Check if there are meetings to mark as completed
+            var meetingsToComplete = await _meetingRepository.GetUncompletedPastMeetings(cutoffTime, cancellationToken);
+
+            //There are no meetings to complete
+            if (!meetingsToComplete.Any()) 
+            {
+                return;
+            }
+
+            //Complete meetings
+            foreach(var meeting in meetingsToComplete)
+            {
+                meeting.MarkAsCompleted();
+            }
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}

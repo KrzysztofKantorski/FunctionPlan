f69c310646fa5e87fc55a1c9f3fc768259829089
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-19 14:16:20 +0200

    (feat): Add background task library

---
+++ FunctionPlan/Infrastructure/BackgroundJob/ClearPastMeetingsJob.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace Infrastructure.BackgroundJob
+{
+    internal sealed class ClearPastMeetingsJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

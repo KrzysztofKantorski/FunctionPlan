67ebe9410c671c0d0c21545b915e2bbc914d6c3d
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-20 13:07:47 +0200

    (feat): Get meetings validator

---
+++ FunctionPlan/Application/Meetings/Queries/GetMeetings/GetMeetingsQueryHandlerValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Application.Meetings.Queries.GetMeetings
+{
+    internal sealed class GetMeetingsQueryHandlerValidator: AbstractValidator<GetMeetingsQuery>
+    {
+        public GetMeetingsQueryHandlerValidator() 
+        {
+            RuleFor(x => x.SearchTerm)
+                .MaximumLength(100).WithMessage("Incorrect search phrase");
+
+            RuleFor(x => x.SortOrder)
+                .Must(x => string.IsNullOrWhiteSpace(x) || x.ToLower() == "asc" || x.ToLower() == "desc")
+                .WithMessage("Incorrect sort order");
+
+            When(x => x.Status != null, () =>
+            {
+                RuleFor(x => x.Status)
+                    .IsInEnum().WithMessage("Incorrect meeting status.");
+            });
+        }
+    }
+}

1a00f142b65d837ec17fb3d28faef4113728825d
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-20 14:36:12 +0200

    (feat): Comment repository

---
+++ FunctionPlan/Domain/Comments/ICommentRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Comments
+{
+    public interface ICommentRepository
+    {
+        Task<Comment?> GetByIdAsync(int id, CancellationToken cancellationToken);
+        void Update(Comment comment);
+        Task AddAsync(Comment comment, CancellationToken cancellationToken = default);
+        void Remove(Comment comment);
+    }
+}

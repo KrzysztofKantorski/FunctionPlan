06c0191e3e762098ce90efe0422479c14bfa6b1c
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-20 13:09:55 +0200

    (feat): Meeting history validator

---
+++ FunctionPlan/Application/Meetings/Queries/GetPastMeetings/GetPastMeetingsQueryHandlerValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+
+namespace Application.Meetings.Queries.GetPastMeetings
+{
+    internal sealed class GetPastMeetingsQueryHandlerValidator: AbstractValidator<GetPastMeetingsQuery>
+    {
+        public GetPastMeetingsQueryHandlerValidator() 
+        {
+            RuleFor(x => x.SearchTerm)
+                .MaximumLength(100).WithMessage("Incorrect search phrase");
+
+            RuleFor(x => x.SortOrder)
+                .Must(x => string.IsNullOrWhiteSpace(x) || x.ToLower() == "asc" || x.ToLower() == "desc")
+                .WithMessage("Incorrect sort order");
+
+            When(x => x.Status != null, () =>
+            {
+                RuleFor(x => x.Status)
+                    .IsInEnum().WithMessage("Incorrect meeting status.");
+            });
+        }
+    }
+}

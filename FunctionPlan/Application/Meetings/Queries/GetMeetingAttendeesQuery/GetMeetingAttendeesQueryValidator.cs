c9f9552ded539614872641bd4ba7ff22aaab679a
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-20 13:14:56 +0200

    (feat): Get meeting attendees validator

---
+++ FunctionPlan/Application/Meetings/Queries/GetMeetingAttendeesQuery/GetMeetingAttendeesQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+
+namespace Application.Meetings.Queries.GetMeetingAttendeesQuery
+{
+    internal sealed class GetMeetingAttendeesQueryValidator :AbstractValidator<GetMeetingAttendeesQuery>
+    {
+        public GetMeetingAttendeesQueryValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(1).WithMessage("Incorrect meeting");
+        }
+    }
+}

011c7cb9b1385a947cb42baa011f418ce10e41ad
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-20 13:16:02 +0200

    (feat): Get meeting by id validator

---
+++ FunctionPlan/Application/Meetings/Queries/GetMeetingById/GetMeetingByIdQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+
+namespace Application.Meetings.Queries.GetMeetingById
+{
+    internal sealed class GetMeetingByIdQueryValidator: AbstractValidator<GetMeetingByIdQuery>
+    {
+        public GetMeetingByIdQueryValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+               .NotEmpty()
+               .GreaterThanOrEqualTo(1).WithMessage("Incorrect meeting");
+        }
+    }
+}

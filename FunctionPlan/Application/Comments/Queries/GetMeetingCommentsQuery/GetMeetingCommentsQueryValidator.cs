fdf81d47401ca8bd56fd5237c960a86e8a2a3802
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-21 20:41:39 +0200

    (feat): Get comments validator

---
+++ FunctionPlan/Application/Comments/Queries/GetMeetingCommentsQuery/GetMeetingCommentsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Comments.Queries.GetMeetingCommentsQuery
+{
+    internal sealed class GetMeetingCommentsQueryValidator: AbstractValidator<GetMeetingCommentsQuery>
+    {
+        public GetMeetingCommentsQueryValidator()
+        {
+            RuleFor(x => x.MeetingId)
+                .NotEmpty().WithMessage("Meeting cannot be empty")
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect Meeting");
+        }
+    }
+}

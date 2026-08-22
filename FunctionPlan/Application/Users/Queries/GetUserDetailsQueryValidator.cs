8bac4871a8fe7c9e5314a8e04517cbe155927ce9
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-22 16:11:42 +0200

    (feat): Get user details validator

---
+++ FunctionPlan/Application/Users/Queries/GetUserDetailsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Queries
+{
+    public sealed class GetUserDetailsQueryValidator: AbstractValidator<GetUserDetailsQuery>
+    {
+        public GetUserDetailsQueryValidator() 
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect user id");
+        }
+    }
+}

b1cc632b943707b4eb15b836e2786b1cf16e28f6
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-22 16:22:22 +0200

    (feat): Get user details endpoint

---
+++ FunctionPlan/API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using API.Extensions;
+using Application.Users.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/users")]
+    [EnableRateLimiting("GlobalLimit")]
+
+    public sealed class UsersController : ControllerBase
+    {
+        private ISender _sender;
+        public UsersController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+
+
+        //Get meeting by id
+        [HttpGet("me")]
+        public async Task<IActionResult> GetMeetingById(
+            CancellationToken cancellationToken
+            )
+        {
+            var command = new GetUserDetailsQuery(User.GetUserId());
+            var result = await _sender.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+
+    }
+}

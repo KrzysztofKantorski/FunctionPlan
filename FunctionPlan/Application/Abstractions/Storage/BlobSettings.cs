46ffe70f2a907c115a2846a2959613da07df57ce
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-24 13:10:42 +0200

    (feat): Blob settings

---
+++ FunctionPlan/Application/Abstractions/Storage/BlobSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Abstractions.Storage
+{
+    public class BlobSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string AvatarsContainerName { get; set; } = string.Empty;
+    }
+}

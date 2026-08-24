18045d746f3ebdbf3dd17c7fad18c5d01ee2f4ab
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-24 12:16:33 +0200

    (feat): Manage user images interface

---
+++ FunctionPlan/Application/Abstractions/Storage/FileResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Abstractions.Storage
+{
+    public sealed record FileResponse(
+        Stream Stream, 
+        string ContentType
+    );
+}
---
+++ FunctionPlan/Application/Abstractions/Storage/IBlobService.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Abstractions.Storage
+{
+    public interface IBlobService
+    {
+        Task UploadFileAsync(Guid fileId, Stream stream, string contentType, CancellationToken cancellationToken);   
+
+        Task<FileResponse> DownloadFileAsync(Guid fileId, CancellationToken cancellationToken);
+
+        Task DeleteFileAsync(Guid fileId, CancellationToken cancellationToken);
+    }
+}

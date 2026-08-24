d086131e556f8ce3a29dfc0b185520cbf4ee9b32
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-24 14:53:23 +0200

    (feat): Upload user image handler

---
+++ FunctionPlan/Application/Users/Commands/UploadUserImage/UploadUserImageCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Commands.UploadUserImage
+{
+    public sealed record UploadUserImageCommand
+    (
+        Stream UploadedImage,
+        int UserId
+    ):ICommand;
+}
---
+++ FunctionPlan/Application/Users/Commands/UploadUserImage/UploadUserImageCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using Application.Abstractions.Storage;
+using Application.Exceptions;
+using Domain.Common;
+using Domain.Users;
+using MediatR;
+
+namespace Application.Users.Commands.UploadUserImage
+{
+    internal sealed class UploadUserImageCommandHandler : IRequestHandler<UploadUserImageCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IBlobService _blobService;
+
+        public UploadUserImageCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork, IBlobService blobService)
+        {
+            _userRepository = userRepository;
+            _blobService = blobService;
+            _unitOfWork = unitOfWork;
+
+        }
+
+        public async Task Handle(UploadUserImageCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId);
+
+            if (user == null) 
+            {
+                throw new UserNotFoundException("User not found");
+            }
+
+            //Generate GUID for filename
+            var fileId = Guid.NewGuid();
+
+
+            //Check if user alerdy uploaded image
+            if (!string.IsNullOrWhiteSpace(user.ProfilePictureUrl)) 
+            {
+                if (Guid.TryParse(user.ProfilePictureUrl, out var oldPictureId)) 
+                {
+                    await _blobService.DeleteFileAsync(oldPictureId, cancellationToken);
+                }
+            }
+
+
+            //Upload image
+            await _blobService.UploadFileAsync(fileId, request.UploadedImage, request.UploadedImage.ContentType, cancellationToken);
+
+
+            //Update metadata
+            user.SetUserImage(fileId.ToString());
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}

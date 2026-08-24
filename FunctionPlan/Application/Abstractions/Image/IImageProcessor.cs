2c1cc2a08a726fd02654fcb7ab49d850ba39a5c1
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-24 12:19:26 +0200

    (feat): Image processing interface

---
+++ FunctionPlan/Application/Abstractions/Image/IImageProcessor.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Abstractions.Image
+{
+    public interface IImageProcessor
+    {
+        Task<Stream> ProcessImageAsync(Stream originalStream, CancellationToken cancellationToken);
+    }
+}

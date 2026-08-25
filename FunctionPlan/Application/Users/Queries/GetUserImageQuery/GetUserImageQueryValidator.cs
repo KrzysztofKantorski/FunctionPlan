290fd75a3e0746636b01fa32cba6b87d22eaf6b9
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-25 13:55:04 +0200

    (feat): Get user image validator

---
+++ FunctionPlan/Application/Users/Queries/GetUserImageQuery/GetUserImageQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+
+namespace Application.Users.Queries.GetUserImageQuery
+{
+    public sealed class GetUserImageQueryValidator: AbstractValidator<GetUserImageQuery>
+    {
+        public GetUserImageQueryValidator() {
+
+            RuleFor(x => x.UserId)
+                .GreaterThanOrEqualTo(0).WithMessage("Incorrect user id");
+        }
+    }
+}

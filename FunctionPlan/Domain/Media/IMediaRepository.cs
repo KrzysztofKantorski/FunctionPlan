9aa637997849b64c2ad0b12233f646367d41c549
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-26 12:58:53 +0200

    (feat): Media repository

---
+++ FunctionPlan/Domain/Media/IMediaRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Media
+{
+    public interface IMediaRepository
+    {
+        Task<Media?> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task AddAsync(Media media, CancellationToken cancellationToken);
+        void UpdateAsync(Media media);
+        void Remove(Media media);
+    }
+}

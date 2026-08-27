2565e29082557a8ab0e6567b3cc6b03b6f95b547
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-27 20:09:54 +0200

    (feat): Get user avatar validator

---
+++ FunctionPlan/Application/Media/Queries/GetUsersAvatars/GetUsersAvatarsValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+
+namespace Application.Media.Queries.GetUsersAvatars
+{
+    public sealed class GetUsersAvatarsValidator: AbstractValidator<GetUsersAvatarsQuery>
+    {
+
+        public GetUsersAvatarsValidator() 
+        {
+            RuleFor(x => x.UserId)
+               .NotEmpty()
+               .GreaterThanOrEqualTo(0).WithMessage("Incorrect user");
+
+            RuleFor(x => x.ImageId)
+                .NotEmpty()
+                .Must(id => Guid.TryParse(id, out _))
+                .WithMessage("Incorrect image id");
+        }
+    }
+}

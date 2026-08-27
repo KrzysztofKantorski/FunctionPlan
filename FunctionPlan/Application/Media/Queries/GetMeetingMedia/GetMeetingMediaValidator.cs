195dada2a1821e5074661a5a8d8f0b498f8e97f3
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-27 13:26:14 +0200

    (feat): Meeting media validator

---
+++ FunctionPlan/Application/Media/Queries/GetMeetingMedia/GetMeetingMediaValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Media.Queries.GetMeetingMedia
+{
+    public sealed class GetMeetingMediaValidator: AbstractValidator<GetMeetingMediaCommand>
+    {
+        public GetMeetingMediaValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+               .NotEmpty()
+               .GreaterThanOrEqualTo(0).WithMessage("Incorrect meeting");
+
+            RuleFor(x => x.UserId)
+               .NotEmpty()
+               .GreaterThanOrEqualTo(0).WithMessage("Incorrect user");
+        }
+    }
+}

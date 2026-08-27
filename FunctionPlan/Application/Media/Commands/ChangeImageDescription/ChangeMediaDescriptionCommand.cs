ef6bab5277420d57f2351df7adfac09ccae91805
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-27 23:15:34 +0200

    (feat): Chenge image description handler

---
+++ FunctionPlan/Application/Media/Commands/ChangeImageDescription/ChangeMediaDescriptionCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Media.Commands.ChangeImageDescription
+{
+    public sealed record ChangeMediaDescriptionCommand
+    (
+        int MeetingId,
+        int UserId,
+        string ImageId
+    ):ICommand;
+}
---
+++ FunctionPlan/Application/Media/Commands/ChangeImageDescription/ChangeMediaDescriptionCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Abstractions.Storage;
+using Application.Exceptions;
+using Domain.Common;
+using Domain.Meetings;
+using MediatR;
+using Microsoft.Extensions.Options;
+
+namespace Application.Media.Commands.ChangeImageDescription
+{
+    internal sealed class ChangeMediaDescriptionCommandHandler : IRequestHandler<ChangeMediaDescriptionCommand>
+    {
+        private readonly IMeetingRepository _meetingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ChangeMediaDescriptionCommandHandler(IMeetingRepository meetingRepository, IBlobService blobService,
+            IOptions<BlobSettings> blobOptions, IUnitOfWork unitOfWork)
+        {
+            _meetingRepository = meetingRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(ChangeMediaDescriptionCommand request, CancellationToken cancellationToken)
+        {
+
+            if (!Guid.TryParse(request.ImageId, out Guid fileId))
+            {
+                throw new Exception("Incorrect image id format.");
+            }
+
+            var meeting = await _meetingRepository.GetByIdWithMediaAsync(request.MeetingId);
+
+            if (meeting == null)
+            {
+                throw new MeetingNotFoundException("Meeting does not exist");
+            }
+
+
+            meeting.UpdateDescription(fileId, request.UserId);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}

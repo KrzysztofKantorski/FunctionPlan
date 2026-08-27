e8d787b1693cfe544e1746cace42c4f415f60fc3
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-27 22:23:15 +0200

    (feat): Remove meeting image handler

---
+++ FunctionPlan/Application/Media/Commands/RemoveMediaFile/RemoveMediaFileCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Media.Commands.RemoveMediaFile
+{
+    public sealed record RemoveMediaFileCommand
+    (
+        int MeetingId,
+        int UserId,
+        string ImageId
+    ):ICommand;
+}
---
+++ FunctionPlan/Application/Media/Commands/RemoveMediaFile/RemoveMediaFileCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using Application.Abstractions.Storage;
+using Application.Exceptions;
+using Domain.Common;
+using Domain.Meetings;
+using MediatR;
+
+namespace Application.Media.Commands.RemoveMediaFile
+{
+    internal sealed class RemoveMediaFileCommandHandler : IRequestHandler<RemoveMediaFileCommand>
+    {
+        private readonly IMeetingRepository _meetingRepository;
+        private readonly IBlobService _blobService;
+        private readonly BlobSettings _blobSettings;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RemoveMediaFileCommandHandler(IMeetingRepository meetingRepository, IBlobService blobService, 
+            BlobSettings blobSettings, IUnitOfWork unitOfWork)
+        {
+            _meetingRepository = meetingRepository;
+            _blobService = blobService;
+            _blobSettings = blobSettings;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(RemoveMediaFileCommand request, CancellationToken cancellationToken)
+        {
+
+            if (!Guid.TryParse(request.ImageId, out Guid fileId))
+            {
+                throw new Exception("Incorrect image id format.");
+            }
+
+            var meeting = await _meetingRepository.GetByIdWithUsersAsync(request.MeetingId);
+
+            if (meeting == null) 
+            {
+                throw new MeetingNotFoundException("Meeting does not exist");
+            }
+
+            meeting.Remove();
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            //Save file to azure storage
+            await _blobService.DeleteFileAsync(
+                _blobSettings.MeetingsContainerName,
+                fileId,
+                cancellationToken);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}

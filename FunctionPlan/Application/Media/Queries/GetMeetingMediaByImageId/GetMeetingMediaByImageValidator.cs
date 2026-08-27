6ee4b76cbbf7515d0201dcd5ee597afcfc6f5330
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-27 15:02:37 +0200

    (feat): Get meeting image validator

---
+++ FunctionPlan/Application/Media/Queries/GetMeetingMediaByImageId/GetMeetingMediaByImageValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Application.Media.Queries.GetMeetingMediaByImageId
+{
+    public sealed class GetMeetingMediaByImageValidator: AbstractValidator<GetMeetingMediaByImageQuery>
+    {
+        public GetMeetingMediaByImageValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+               .NotEmpty()
+               .GreaterThanOrEqualTo(0).WithMessage("Incorrect meeting");
+
+            RuleFor(x => x.UserId)
+               .NotEmpty()
+               .GreaterThanOrEqualTo(0).WithMessage("Incorrect user");
+
+            RuleFor(x => x.ImageId)
+                .NotEmpty()
+                .Must(id => Guid.TryParse(id, out _))
+                .WithMessage("Incorrect image id");
+        }
+    }
+}

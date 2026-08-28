251b57f9b0024226ba94030fc82d18bf9f73b762
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-28 14:57:22 +0200

    (feat): Meeting media validators

---
+++ FunctionPlan/Application/Media/Commands/ChangeImageDescription/ChangeMediaDescriptionValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Media.Commands.ChangeImageDescription
+{
+    public sealed class ChangeMediaDescriptionValidator: AbstractValidator<ChangeMediaDescriptionCommand>
+    {
+        public ChangeMediaDescriptionValidator() 
+        {
+            RuleFor(x => x.MeetingId).GreaterThan(0);
+            RuleFor(x => x.UserId).GreaterThan(0);
+
+            RuleFor(x => x.Description)
+                .MaximumLength(200).WithMessage("Too long description.");
+        }
+    }
+}
---
+++ FunctionPlan/Application/Media/Commands/RemoveMediaFile/RemoveMediaFileValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Application.Media.Commands.RemoveMediaFile
+{
+    public sealed class RemoveMediaFileValidator: AbstractValidator<RemoveMediaFileCommand>
+    {
+        public RemoveMediaFileValidator() 
+        {
+            RuleFor(x => x.MeetingId)
+              .NotEmpty()
+              .GreaterThanOrEqualTo(0).WithMessage("Incorrect meeting");
+
+            RuleFor(x => x.UserId)
+               .NotEmpty()
+               .GreaterThanOrEqualTo(0).WithMessage("Incorrect user");
+
+            RuleFor(x => x.ImageId)
+                .NotEmpty()
+                .Must(id => Guid.TryParse(id, out _))
+                .WithMessage("Incorrect image id");
+        }
+    }
+}

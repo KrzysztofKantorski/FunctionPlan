8e5a6a5790852f0f80db0a40cf23c1bb719b001a
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-08-31 14:55:19 +0200

    (feat): Google auth validator

---
+++ FunctionPlan/Application/Auth/Commands/LoginWithGoogle/LoginWithGoogleCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+
+namespace Application.Auth.Commands.LoginWithGoogle
+{
+    public sealed class LoginWithGoogleCommandValidator :AbstractValidator<LoginWithGoogleCommand>
+    {
+        public LoginWithGoogleCommandValidator() {
+
+            RuleFor(x => x.GoogleIdToken)
+                    .NotEmpty()
+                    .MinimumLength(100)
+
+                    //JWT must have 3 parts
+                    .Matches(@"^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$")
+                    .WithMessage("Incorrect token format.");
+
+        }
+    }
+}

5b30ee18b5e41dd3bfd1756a1cf14169a7496801
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-09 12:58:06 +0200

    (feat): login service

---
+++ ClientApp/src/app/core/auth/login-service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { LoginRequest, LoginResponse } from '../models/login-models';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService 
+{
+
+  private apiUrl = 'https://localhost:7206';
+  private httpClient = inject(HttpClient);
+
+  login(credentials: LoginRequest)
+  {
+    return this.httpClient.post<LoginResponse>(`${this.apiUrl}/auth/login`, credentials, {withCredentials: true}).pipe(
+
+      tap(response=>
+      {
+        //Save access token to local storage
+        localStorage.setItem('access_token', response.token);
+      })
+    )
+  }
+
+  getToken(): string | null
+  {
+    return localStorage.getItem('access_token');
+  }
+}

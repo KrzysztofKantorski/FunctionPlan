5b499350aab682f4231f7ea9e9655b370b370ded
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-09 15:33:59 +0200

    (feat): Use guest guard

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -4,11 +4,31 @@ import {LoginPage} from './features/auth/login-page/login-page';
 import {RegisterPage} from './features/auth/register-page/register-page';
 import {VerifyPage} from './features/auth/verify-page/verify-page';
 import {MainPage} from './features/meetings/main-page/main-page';
+import { guestGuard } from './core/guards/guest-guard';
 
 export const routes: Routes = [
-    {path :"", component: WelcomePage},
-    {path :"login", component: LoginPage},
-    {path :"register", component: RegisterPage},
-    {path :"verify", component: VerifyPage},
-    {path:"main-page", component: MainPage}
+    {
+        path :"", 
+        component: WelcomePage,
+        canActivate: [guestGuard]
+    },
+    {
+        path :"login",
+        component: LoginPage,
+        canActivate: [guestGuard]
+    },
+    {
+        path :"register", 
+        component: RegisterPage,
+        canActivate: [guestGuard]
+    },
+    {
+        path :"verify", 
+        component: VerifyPage,
+        canActivate: [guestGuard]
+    },
+    {
+        path:"main-page", 
+        component: MainPage
+    }
 ];

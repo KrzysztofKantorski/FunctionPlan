65b52ece47ff5018979fc381f089b1a8f8ec243d
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-09 12:18:18 +0200

    (feat): Register error handlers

--- ClientApp/src/app/app.config.ts
+++ ClientApp/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptor/error-interceptor';
+import { GlobalErrorHandler } from './core/handlers/global-error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    { provide: ErrorHandler, useClass: GlobalErrorHandler },
   ]
 };

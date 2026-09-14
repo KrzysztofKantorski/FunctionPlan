b7277aebd6f1d4d25bbdee749f51787556c01fb8
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-14 14:23:34 +0200

    (feat): Sidebar design

--- ClientApp/src/app/features/meetings/main-page/main-page.ts
+++ ClientApp/src/app/features/meetings/main-page/main-page.ts
@@ -6,9 +6,12 @@ import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { Navbar } from '../../../shared/components/navbar/navbar';
 import { NavbarBtnGroup } from '../../../shared/components/navbar-btn-group/navbar-btn-group';
+import { Sidebar } from '../../../shared/components/sidebar/sidebar';
+import { MeetingFilters } from '../../../core/models/meeting-filters';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-main-page',
-  imports: [MainHeader, AsyncPipe, Navbar, NavbarBtnGroup],
+  imports: [MainHeader, AsyncPipe, Navbar, NavbarBtnGroup, Sidebar, MatButtonModule],
   templateUrl: './main-page.html'
 })
 export class MainPage {
@@ -19,5 +22,11 @@ export class MainPage {
   ngOnInit(): void
   {
     this.userProfile$ = this.userService.getUserDetails();
+  }
+
+
+  onFiltersUpdated(filters: MeetingFilters)
+  {
+      console.log('Filters changed:', filters);
   }
 }
--- ClientApp/src/app/shared/components/sidebar/sidebar.ts
+++ ClientApp/src/app/shared/components/sidebar/sidebar.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import {provideNativeDateAdapter} from '@angular/material/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MeetingFilters } from '../../../core/models/meeting-filters';
+import { debounce, debounceTime, distinctUntilChanged } from 'rxjs';
+
 @Component({
   selector: 'sidebar',
   imports: [
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatDatepickerModule],
+    MatDatepickerModule,
+  ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './sidebar.html'
 })
-export class Sidebar {}
+export class Sidebar {
+
+  //form builder 
+  private fb = inject(FormBuilder);
+
+  //Emit filters to meeting component
+  @Output() filtersChanged = new EventEmitter<MeetingFilters>();
+
+  filterForm = this.fb.group
+  ({
+    startDate: [null as Date | null],
+    endDate: [null as Date | null],
+    sortOrder: ['asc'],
+    status: [null as number | null]
+  });
+
+  ngOnInit()
+  {
+    this.filterForm.valueChanges.pipe(
+
+      //Add small delay
+      debounceTime(300),
+
+      //Check if changes were made
+      distinctUntilChanged((prev, curr) => JSON.stringify(prev) === JSON.stringify(curr))
+
+    ).subscribe(value =>{
+      this.filtersChanged.emit(value as MeetingFilters)
+    })
+  }
+}

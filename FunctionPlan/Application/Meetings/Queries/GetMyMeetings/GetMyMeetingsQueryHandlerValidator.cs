b79634e421502a4a35f9c16158c85bd324522932
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-20 14:00:41 +0200

    (feat): Add validator

---
+++ FunctionPlan/Application/Meetings/Queries/GetMyMeetings/GetMyMeetingsQueryHandlerValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Validators;
+using FluentValidation;
+
+namespace Application.Meetings.Queries.GetMyMeetings
+{
+    internal sealed class GetMyMeetingsQueryHandlerValidator: AbstractValidator<GetMyMeetingsQuery>
+    {
+        public GetMyMeetingsQueryHandlerValidator() 
+        {
+            RuleFor(x => x.Filters)
+                 .SetValidator(new MeetingFiltersDtoValidator());
+            RuleFor(x => x.userId)
+                .NotEmpty().WithMessage("Invalid user id");
+        }
+    }
+}

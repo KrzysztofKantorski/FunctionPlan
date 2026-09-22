085a80f8485b7b4f1b9620f9ee196299d85d15c4
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-22 15:22:12 +0200

    (feat): Save user state

--- ClientApp/src/app/app.ts
+++ ClientApp/src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatSlideToggle} from '@angular/material/slide-toggle';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { AuthService } from './core/auth/auth-service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MatSlideToggle, MatCardModule, MatButtonModule],
@@ -11,4 +12,14 @@ import {MatCardModule} from '@angular/material/card';
 })
 export class App {
   protected readonly title = signal('ClientApp');
+  private authService = inject(AuthService)
+
+  ngOnInit(): void {
+    if (this.authService.hasToken()) {
+      this.authService.fetchCurrentUser().subscribe({
+        //Logout if token expired or is incorrect
+        error: () => this.authService.logout() 
+      });
+    }
+  }
 }

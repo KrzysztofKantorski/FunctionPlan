d47435952892515ba4c89bfe3edc48419744c774
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-24 15:01:43 +0200

    (feat): Get meeting media

---
+++ ClientApp/src/app/core/services/media-service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { MeetingMediaUrlsResponse } from '../models/media/meetingMediaUrlsResponse';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MediaService 
+{
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
+  
+
+  //Get meeting media info (image url, description, url)
+  getMeetingMedia(meetingId: number): Observable<MeetingMediaUrlsResponse[]>
+  {
+    return this.http.get<MeetingMediaUrlsResponse[]>(`${this.apiUrl}/meetings/${meetingId}/media`)
+  }
+}

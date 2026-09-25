238dcaaea1d5333a8bcf54b1f8c461ec85b4023b
KrzysztofKantorski <krzysiu8845@wp.pl>
2026-09-25 21:32:22 +0200

    (feat): Health chech endpoint

---
+++ FunctionPlan/API/Controllers/HelalthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HelalthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok(new { status= "Healthy" });
+        }
+
+    }
+}
